5c51a6dc7310de55ad4e964a414b4e0fa273121e
Cris Jerez <crisjerez102@gmail.com>
2024-10-06 21:57:34 +0800

    fix some logic

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -13,26 +13,17 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { redirect } from "next/navigation";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/navigation";
 
 export function LoginForm() {
   const [username, setUsername] = useState("admin");
   const [password, setPassword] = useState("admin");
   const [agreed, setAgreed] = useState(false);
-
+  const router = useRouter();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (agreed) {
-      redirect("/dashboard");
-      // console.log("Signing in...");
-    } else {
-      alert("Please agree to the terms and policies before signing in.");
-    }
-  };
-
-  const handleSignIn = () => {
-    useRouter().push('/dashboard');
+    const loginUser = 
+    router.push("/dashboard");
   };
 
   return (
@@ -41,11 +32,10 @@ export function LoginForm() {
         <Image
           src="/background.jpg"
           alt="Background"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          priority
-          className="blur-sm"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className=" blur-sm object-cover"
         />
       </div>
       <div className="w-full max-w-[500px] relative z-10 bg-white p-8 rounded-lg shadow-lg">
@@ -117,7 +107,6 @@ export function LoginForm() {
             className="w-full bg-blue-600 hover:bg-blue-700"
             type="submit"
             disabled={!agreed}
-            onClick={handleSignIn}
           >
             Sign In
           </Button>
